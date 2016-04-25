1dd5ee6e7e4d3ddd609dd140cfa8c013fd8ab5f4
scharlatan <a.regenfuss@gmx.de>
2016-04-25 17:18:01 +0200

    First working version of treat, some little issues, but nothing that can't be fixed.

--- splitup.lua
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env lua
-
--- analyse the arguments, -s SEP specifies the separator
--- read a line
--- split it up into different fields, using the given separator
--- write each field into the corresponding fifo, the nth+1 argument
